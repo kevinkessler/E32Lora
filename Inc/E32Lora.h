376067b4ae2adc5ad5c78ffaa0ec0437ba995fd8
Kevin Kessler <kkessler@kkessler.com>
2019-01-10 00:58:02 -0500

    All the setters

--- Inc/E32Lora.h
+++ Inc/E32Lora.h
@@ -17,18 +17,69 @@
 #define E32_OK 0
 #define E32_TIMEOUT 1
 #define E32_ERROR 100
+#define E32_MESSAGE_TOO_LONG 101
 
 typedef uint8_t E32_STATUS;
+
 enum uartBaud {
-	B1200 = 0,
-	B2400 = 1,
-	B4800 = 2,
-	B9600 = 3,
-	B19200 = 4,
-	B38400 = 5,
-	B57600 = 6,
-	B115200 = 7
+	Baud_1200 = 0,
+	Baud_2400 = 1,
+	Baud_4800 = 2,
+	Baud_9600 = 3,
+	Baud_19200 = 4,
+	Baud_38400 = 5,
+	Baud_57600 = 6,
+	Baud_115200 = 7
 };
+
+enum uartParity {
+	Parity_8N1 = 0,
+	Parity_8O1 = 1,
+	Parity_8E1 = 2
+};
+
+enum airRate {
+	AirRate_0300 = 0,
+	AirRate_1200 = 1,
+	AirRate_2400 = 2,
+	AirRate_4800 = 3,
+	AirRate_9600 = 4,
+	AirRate_19200 = 5
+};
+
+enum wakeupTime {
+	Wakeup_250 = 0,
+	Wakeup_500 = 1,
+	Wakeup_750 = 2,
+	Wakeup_1000 = 3,
+	Wakeup_1250 = 4,
+	Wakeup_1500 = 5,
+	Wakeup_1750 = 6,
+	Wakeup_2000 = 7
+};
+
+enum txPower {
+	TxPwr_20 = 0,
+	TxPwr_17 = 1,
+	TxPwr_14 = 2,
+	TxPwr_10 = 3
+};
+
+enum txMode {
+	TxMode_Transparent = 0,
+	TxMode_Fixed = 1
+};
+
+enum ioMode {
+	IOMode_Open = 0,
+	IOMode_Pull = 1
+};
+
+enum fecSwitch {
+	FEC_Off = 0,
+	FEC_On = 1
+};
+
 E32_STATUS E32_Init(GPIO_TypeDef* portM0, uint16_t pinM0, GPIO_TypeDef* portM1, uint16_t pinM1,
 		GPIO_TypeDef* portAux, uint16_t pinAux, UART_HandleTypeDef *h);
 E32_STATUS E32_SetMode(uint8_t mode);
@@ -37,8 +88,20 @@ E32_STATUS E32_GetConfig(uint8_t *configBuffer);
 E32_STATUS E32_GetVersion(uint8_t *configBuffer);
 E32_STATUS E32_Reset(void);
 E32_STATUS E32_SaveParams(void);
-
-
+E32_STATUS E32_SetAddress(uint16_t addr);
+E32_STATUS E32_SetParity(enum uartParity parity);
+E32_STATUS E32_SetUartBaud(enum uartBaud baud);
+E32_STATUS E32_SetAirRate(enum airRate rate);
+E32_STATUS E32_SetChannel(uint8_t channel);
+E32_STATUS E32_SetTransmissionMode(enum txMode mode);
+E32_STATUS E32_SetIOnMode(enum ioMode mode);
+E32_STATUS E32_SetIOMode(enum ioMode mode);
+E32_STATUS E32_SetWakeTime(enum wakeupTime wake);
+E32_STATUS E32_SetFECSwitch(enum fecSwitch fec);
+E32_STATUS E32_SetTXPower(enum txPower power);
+E32_STATUS E32_SetTargetChannel(uint8_t channel);
+E32_STATUS E32_SetTargetAddress(uint8_t addr);
+E32_STATUS E32_Transmit(uint8_t *message, uint16_t length);
 
 void E32_Poll(void);
 
--- Src/E32Lora.c
+++ Src/E32Lora.c
@@ -8,6 +8,7 @@
 #include "main.h"
 #include "E32Lora.h"
 #include <stdio.h>
+#include <string.h>
 #include "stm32l432xx.h"
 
 static UART_HandleTypeDef *huart;
@@ -17,7 +18,10 @@ static GPIO_TypeDef* m1Port;
 static uint16_t m1Pin;
 static GPIO_TypeDef* auxPort;
 static uint16_t auxPin;
-static uint16_t baudRate;
+static uint32_t baudRateList[] = {1200,2400,4800,9600,19200,38400,57600,115200};
+static uint8_t targetChannel;
+static uint16_t targetAddress;
+static uint8_t currentConfig[6];
 
 static E32_STATUS E32_WaitForAux(uint8_t state)
 {
@@ -77,6 +81,10 @@ static E32_STATUS E32_ConfigRequest(uint8_t *request, uint8_t requestLength,
 	return E32_OK;
 }
 
+static uint32_t E32_GetBaud() {
+	return baudRateList[(currentConfig[3] & 0xC7) >> 3];
+}
+
 E32_STATUS E32_Init(GPIO_TypeDef* portM0, uint16_t pinM0, GPIO_TypeDef* portM1, uint16_t pinM1,
 		GPIO_TypeDef* portAux, uint16_t pinAux, UART_HandleTypeDef *h)
 {
@@ -91,7 +99,10 @@ E32_STATUS E32_Init(GPIO_TypeDef* portM0, uint16_t pinM0, GPIO_TypeDef* portM1, 
 	auxPort = portAux;
 	auxPin = pinAux;
 
-	baudRate = 9600;
+	E32_STATUS error;
+	uint8_t dummy[6];
+	if((error = E32_GetConfig(dummy)) != E32_OK)
+		return error;
 
 	return E32_SetMode(NORMAL_MODE);
 }
@@ -113,7 +124,7 @@ E32_STATUS E32_SetMode(uint8_t mode)
 	if (mode == SLEEP_MODE)
 		huart->Init.BaudRate = 9600;
 	else
-		huart->Init.BaudRate = baudRate;
+		huart->Init.BaudRate = E32_GetBaud();
 
 	HAL_UART_Init(huart);
 	HAL_Delay(50);
@@ -131,7 +142,11 @@ E32_STATUS E32_GetConfig(uint8_t *configBuffer)
 {
 	uint8_t message[]={0xc1, 0xc1, 0xc1 };
 
-	return E32_ConfigRequest(message, 3, configBuffer, 6);
+	E32_STATUS retval = E32_ConfigRequest(message, 3, configBuffer, 6);
+	if (retval == E32_OK)
+		memcpy(currentConfig, configBuffer, 6);
+
+	return retval;
 }
 
 E32_STATUS E32_GetVersion(uint8_t *configBuffer)
@@ -147,16 +162,16 @@ E32_STATUS E32_Reset()
 	E32_STATUS error;
 
 	if((error = E32_ConfigRequest(message,3,NULL,0)) != E32_OK)
-		return 0x10;
+		return error;
 
 	if ((error=E32_WaitForAux(0)) != E32_OK)
-		return 0x11;
+		return error;
 
 	if ((error=E32_WaitForAux(1)) != E32_OK)
-		return 0x12;
+		return error;
 
 	if((error = E32_SetMode(NORMAL_MODE)) != E32_OK)
-		return 0x13;
+		return error;
 
 	return E32_OK;
 }
@@ -175,8 +190,221 @@ E32_STATUS E32_SaveParams()
 	return E32_OK;
 }
 
+E32_STATUS E32_SetAddress(uint16_t addr) {
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[1] = (addr & 0xff00) >> 8;
+	config[2] = addr & 0xff;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetParity(enum uartParity parity) {
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[3] = (config[3] & 0x3f) | parity << 6;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+
+}
+
 E32_STATUS E32_SetUartBaud(enum uartBaud baud)
 {
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[3] = (config[3] & 0xc7) | baud << 3;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetAirRate(enum airRate rate)
+{
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[3] = (config[3] & 0xF8) | rate;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetChannel(uint8_t channel)
+{
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[4] = channel & 0x1F;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetTransmissionMode(enum txMode mode)
+{
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[5] = (config[5] & 0x7F) | mode << 7;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetIOnMode(enum ioMode mode)
+{
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[5] = (config[5] & 0xBF) | mode << 6;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetIOMode(enum ioMode mode)
+{
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[5] = (config[5] & 0xBF) | mode << 6;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetWakeTime(enum wakeupTime wake) {
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[5] = (config[5] & 0xC7) | wake << 3;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetFECSwitch(enum fecSwitch fec) {
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[5] = (config[5] & 0xFB) | fec << 2;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetTXPower(enum txPower power) {
+	uint8_t config[6], resp[6];
+	E32_STATUS error;
+
+	if((error=E32_GetConfig(config)) != E32_OK)
+		return error;
+
+	config[0] = 0xc2;
+	config[5] = (config[5] & 0xFC) | power;
+
+	if ((error=E32_ConfigRequest(config, 6 , resp, 6)) != E32_OK)
+		return error;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetTargetChannel(uint8_t channel) {
+	targetChannel = channel & 0x1F;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_SetTargetAddress(uint8_t addr) {
+	targetAddress = addr;
+
+	return E32_OK;
+}
+
+E32_STATUS E32_Transmit(uint8_t *message, uint16_t length) {
+	if (length > 512)
+		return E32_MESSAGE_TOO_LONG;
+
+	if (currentConfig[5] & 0x7F) {
+		if(length > 509)
+			return E32_MESSAGE_TOO_LONG;
+		uint8_t header[3 + length];
+		header[0] = (targetAddress & 0xFF) >> 8;
+		header[1] = targetAddress &0xff;
+		header[2] = targetChannel;
+		memcpy(&header[3],message,length);
+		if(HAL_UART_Transmit(huart, header, length+3, 2000) != HAL_OK)
+			return E32_ERROR;
+	}
+	else
+		if(HAL_UART_Transmit(huart, message, length, 2000) != HAL_OK)
+			return E32_ERROR;
+
 	return E32_OK;
 }
 
@@ -184,7 +412,7 @@ void E32_Poll()
 {
 
 
-	E32_SetUartBaud(B9600);
+	E32_SetUartBaud(Baud_9600);
 	  uint8_t recv[] = {0x01,0x02,0x03,0x04,0x05,0x06};
 	  printf("----------\r\n");
 	  E32_STATUS status= E32_GetConfig(recv);
